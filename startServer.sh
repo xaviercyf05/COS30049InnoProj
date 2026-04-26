2418e9c470bca652fe286acff69ef28f7b83d324
xaviercyf05 <104391384@students.swinburne.edu.my>
2026-04-27 03:21:46 +0800

    startup script update again

--- startServer.sh
+++ startServer.sh
@@ -10,8 +10,10 @@ npm install
 # Start the server
 if pm2 describe innopapp-api > /dev/null 2>&1; then
 pm2 restart innopapp-api --update-env
+echo "Innovation Project Server already exists and was successfully restarted at $(date)" >> /var/log/startup.log
 else
 pm2 start src/server.js --name innopapp-api --update-env
+echo "Innovation Project Server successfully started at $(date)" >> /var/log/startup.log
 fi
 
 pm2 save

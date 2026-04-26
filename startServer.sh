90739192d7cd87b958f09bc3549082405475bbf3
xaviercyf05 <104391384@students.swinburne.edu.my>
2026-04-27 03:05:55 +0800

    startup script update

--- startServer.sh
+++ startServer.sh
@@ -4,15 +4,14 @@
 set -euo pipefail
 
 # Enter file directory and check for updates
-cd /home/xavier/COS30049InnoProj/COS30049InnoProj
-git pull
+git pull --ff-only
 npm install
 
 # Start the server
 if pm2 describe innopapp-api > /dev/null 2>&1; then
 pm2 restart innopapp-api --update-env
 else
-pm2 start server.js --name innopapp-api --update-env
+pm2 start src/server.js --name innopapp-api --update-env
 fi
 
 pm2 save

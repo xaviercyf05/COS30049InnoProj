21041c025b9c7ff74719054c81f3409e7a931690
xaviercyf05 <104391384@students.swinburne.edu.my>
2026-05-24 20:05:18 +0800

    test fix 1

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -24,3 +24,10 @@ jest.mock('react-native-safe-area-context', () => ({
   SafeAreaProvider: ({ children }) => children,
   useSafeAreaInsets: () => ({ top: 0, left: 0, right: 0, bottom: 0 }),
 }));
+
+jest.mock('expo-file-system', () => ({
+  EncodingType: {
+    UTF8: 'utf8',
+  },
+  readAsStringAsync: jest.fn(),
+}));
